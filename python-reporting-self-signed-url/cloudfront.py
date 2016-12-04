aead7dd213704f7195e3269832c7e90cb1aef23b
root <root@ip-172-31-44-81.ec2.internal>
2016-12-04 16:07:00 +0000

    adding report and self signed url

---
+++ python-reporting-self-signed-url/cloudfront.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import time,boto,rsa
+from boto import cloudfront
+from boto.cloudfront import distribution
+
+#AWS_ACCESS_KEY_ID="your access key"
+#AWS_SECRET_ACCESS_KEY="your secret access key"
+
+
+conn = boto.cloudfront.CloudFrontConnection()
+dist = conn.get_all_distributions()
+a=dist[0].get_distribution()
+#Set parameters for URL
+key_pair_id = "XXXXXXXXXXXXX" #cloudfront security key
+priv_key_file = "XXXXXXXXXX.pem" #cloudfront private keypair file
+expires = int(time.time()) + 60 #1 min
+url="https://XXXXXXX.cloudfront.net/XXXXXX"
+signed_url = a.create_signed_url(url, key_pair_id, expires, private_key_file=priv_key_file)
+print signed_url
